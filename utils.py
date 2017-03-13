fe07357cb7efdce57a2029a8df8fe3e306d29aa5
LucianAlexandru <alexandru_f_lucian@yahoo.com>
2017-03-14 01:05:36 +0200

    Update

---
+++ utils.py
@@ -0,0 +1,14 @@
+import Crypto.PublicKey.RSA as RSA
+import hashlib
+
+ssl_certfile = "cert.pem"
+ssl_keyfile = "key.pem"
+
+def generateSignature (data, privateKey):
+    pass
+
+def verifySignature (data, publicKey, signature):
+    pass
+
+def chainHash (data, i):
+    pass
