f625a685e820594c53954139d7553089fe5d8097
LucianAlexandru <alexandru_f_lucian@yahoo.com>
2017-03-14 10:35:22 +0200

    Vendor payment

--- vendor.py
+++ vendor.py
@@ -40,14 +40,18 @@ class Vendor:
             print "Payment registered"
 
             try:
-                commit = self.knownCustomers[port]
+                commit = self.knownCustomers[port][0]
                 commit.isValid()
+                commit.getPayment (request['Data']['Link'], request['Data']['Amount'])
             except AssertionError:
                 connstream.send(
                     json.dumps({'Response': 'Error', 'Data': 'Commit Expired', 'Signature': ''}).encode('utf-8'))
             except KeyError:
                 connstream.send(
                     json.dumps({'Response': 'Error', 'Data': 'Commit Missing', 'Signature': ''}).encode('utf-8'))
+            except PaymentError:
+                connstream.send(
+                    json.dumps({'Response': 'Error', 'Data': 'Invalid Payment', 'Signature': ''}).encode('utf-8'))
             else:
                 connstream.send(json.dumps({'Response': 'OK', 'Data': 'Payment Completed', 'Signature':''}).encode('utf-8'))
 
@@ -57,7 +61,7 @@ class Vendor:
         while True:
             conn, addr = self.soc.accept()
             thread = Thread(target=self.deal_with_client,
-                            args=(conn, addr))
+                            args=(conn, addr[1]))
             thread.start()
 
     def runcmd(self):
@@ -68,11 +72,45 @@ class Vendor:
             cmd = cmd.split(' ')
 
             if cmd[0] == 'redeem':
-                print "now i'm redeeming"
 
-            if cmd[0] == 'exit':
+                try:
+                    consumer = int(cmd[1])
+                    commitList = self.knownCustomers[consumer]
+                except ValueError:
+                    print ('Invalid Command')
+                    continue
+                except KeyError:
+                    print ('No payments for user ' + consumer)
+
+                for commit in commitList:
+                    data = {'Certificate' : commit['Certificate'],
+                            'CertificateSignature' : commit['Certificate']['KeyBroker'],
+                            'Hash' : commit.lastLink,
+                            'Amount' : commit.amount}
+                    response = utils.getResponse (commit.commit['Broker'], 'Redeem', data, '')
+                    print ('Trying to redeem ' + commit.amount + ' from certificate ' + str(commit['Certificate']))
+                    if response['Response'] == 'OK':
+                        print ('Redeem successful')
+                    else:
+                        print ('Redeem failed ' + response['Data'])
+
+            elif cmd[0] == 'clear':
+
+                try:
+                    consumer = int(cmd[1])
+                except ValueError:
+                    print ('Invalid Command')
+                    continue
+
+                if consumer in self.knownCustomers:
+                    del self.knownCustomers[consumer]
+
+            elif cmd[0] == 'exit':
                 print "Okay, bye!"
                 sys.exit()
+
+            else:
+                print ("Unknown Command")
 
 
 class CommittedConsumer:
@@ -80,7 +118,8 @@ class CommittedConsumer:
     def __init__(self, vendor, commit, signature):
 
         self.commit = commit
-        self.hashRoot = commit['HashRoot']
+        self.lastLink = commit['HashRoot']
+        self.amount = 0
 
         assert self.commit['Vendor'] == vendor
         assert utils.verifySignature(self.commit, self.commit[
@@ -95,7 +134,15 @@ class CommittedConsumer:
             'Certificate']['ExpirationDate']
 
     def getPayment(self, link, amount):
-        pass
+        if utils.chainHash(link, amount) != self.lastLink:
+            raise PaymentError
+        self.lastLink = link
+        self.amount += amount
+
+
+class PaymentError (RuntimeError):
+    def __init__(self, arg):
+        self.arg = arg
 
 
 if __name__ == "__main__":
